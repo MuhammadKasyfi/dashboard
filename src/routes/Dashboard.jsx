064b263b6d9ae858e2e64e4dd5f8a63c25a61e60
Kasyfi <kasyfikh@gmail.com>
2025-10-30 13:35:04 +0800

    Read Excel and Parse into Bar Chart

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import * as XLSX from 'xlsx'
 import FileUpload from '../components/FileUpload'
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Bar, Doughnut, Line } from 'react-chartjs-2'
 
 export default function Dashboard() {
   const [data, setData] = useState([])
@@ -18,7 +20,7 @@ export default function Dashboard() {
       const workbook = XLSX.read(data, { type: 'binary' })
       const sheetName = workbook.SheetNames[0] // Assuming we want the first sheet
       const sheet = workbook.Sheets[sheetName]
-      const parsedData = XLSX.utils.sheet_to_json(sheet, { header: 1 })
+      const parsedData = XLSX.utils.sheet_to_json(sheet)
       console.log("Parsed data:", parsedData)
 
       setData(parsedData)
@@ -55,6 +57,26 @@ export default function Dashboard() {
           </tbody>
         </table>
       )}
+      <div className='border mt-4 p-4 col-span-2 w-1/2 h-96'>
+        Chart 1
+        <div className='h-80 w-full'>
+          <Bar 
+            data={{
+              labels: data.map((row) => row[Object.keys(data[0])[1]]), // Extract column 2 dynamically
+              datasets: [
+                {
+                  label: 'Values',
+                  data: data.map((row) => row[Object.keys(data[0])[2]]), // Extract column 2 dynamically
+                  backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                  borderColor: 'rgba(75, 192, 192, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            }}
+            options={{ responsive: true, maintainAspectRatio: false }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
